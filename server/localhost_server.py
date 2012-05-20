ee204a1f29169894dc70a81b171b516df9aa398e
Alexandr Pokutnik <apokutnik@cogniance.com>
2012-05-21 01:43:53 +0300

    rm pyc files

--- server/localhost_server.py
+++ server/localhost_server.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import sys
+import posixpath
+import urllib
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import BaseHTTPServer
+from chisel import is_html
 
 class SonaHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwsrgs):
+    def __init__(self, *args, **kwargs):
         super(SonaHandler, self).__init__(*args, **kwargs)
         self.extensions_map.update({
             '.css': 'text/css',
@@ -23,13 +26,31 @@ class SonaHandler(SimpleHTTPRequestHandler):
             '.mp3': 'audio/mpeg',
             })
 
+    def translate_path(self, serve_path):
+        """
+        The default behavior of SimpleHTTPRequestHandler.translate_path
+        is to serve files from the current directory and all of the
+        directories below it. This class overrides the method and
+        looks for a `rootpath` attribute, which will be used instead
+        of the current directory.
+        """
+        # abandon query parameters
+        path = serve_path.split('?', 1)[0]
+        path = path.split('#', 1)[0]
+        path = posixpath.normpath(urllib.unquote(path))
+        print serve_path, path
+        if is_html(path):
+            print ">>>> YAY", path
+        return super(SonaHandler, self).translate_path(serve_path)
+
 if __name__ == '__main__':
     if sys.argv[1:]:
         port = int(sys.argv[1])
     else:
         port = 80
     server_address = ('', port)
-    httpd = BaseHTTPServer.HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd = BaseHTTPServer.HTTPServer(server_address, SonaHandler)
     sa = httpd.socket.getsockname()
     print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    print "Open  http://%s:%s/" % (sa[0], sa[1])
     httpd.serve_forever()
