7a272d2dddf25adafbe829b414b4219c4eac2d56
Alexandr Pokutnik <apokutnik@cogniance.com>
2012-05-21 00:53:07 +0300

    add chisel based page generator

--- localhost8080.bat
+++ localhost8080.bat
@@ -1,1 +1,1 @@
-python localhost_server.py 8080
+python server/localhost_server.py 8080
---
+++ server/chisel.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+
+# Based on Chisel by David Zhou
+#
+# Requires:
+# jinja2
+
+import sys
+import os
+
+# Settings
+SOURCE = "./"  # end with slash
+DESTINATION = "./export/"  # end with slash
+TEMPLATE_PATH = "./templates/"
+TEMPLATE_OPTIONS = {}
+
+try:
+    import jinja2
+
+    jinja2_env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(TEMPLATE_PATH),
+            **TEMPLATE_OPTIONS)
+
+    def renderTemplate(template, context):
+        template = template.decode("utf8")
+        return jinja2_env.from_string(template).render(context)
+
+    def templateFilter(func):
+        """ decorator to easily create jinja2 filters
+        """
+        jinja2_env.filters[func.__name__] = func
+
+except ImportError:
+    def renderTemplate(template, context):
+        raise NotImplementedError('no template engine configured!')
+
+
+_STEPS = []
+
+
+def step(func):
+    def wrapper(*args, **kwargs):
+        print
+        print "Starting " + func.__name__ + "...",
+        func(*args, **kwargs)
+        print "Done."
+    _STEPS.append(wrapper)
+    return wrapper
+
+
+def is_dot(filename):
+    return filename and filename[0] == '.'
+
+
+def is_sub(parent, subfolder):
+    return os.path.abspath(subfolder).startswith(
+                os.path.abspath(parent))
+
+
+def can_skip(filename):
+    if is_sub(TEMPLATE_PATH, filename):
+        return True
+    if is_sub(os.path.dirname(DESTINATION), filename):
+        return True
+    if is_sub(os.path.dirname(__file__), filename):
+        return True
+
+
+def ensure_destination():
+    if not os.path.exists(DESTINATION):
+        os.makedirs(DESTINATION)
+
+
+def get_tree(source):
+    files = []
+    rel = lambda fname: fname[len(source):]
+
+    for root, ds, fs in os.walk(source):
+        print 'root>', root, can_skip(root)
+        if is_dot(rel(root)) or can_skip(root):
+            continue
+        for name in fs:
+            if is_dot(name):
+                continue
+            path = os.path.join(root, name)
+            rel_path = rel(path)
+            print "+", rel_path
+            mtime = os.path.getmtime(path)
+            with open(path, "rU") as f:
+                files.append({
+                    'content': ''.join(f.readlines()),
+                    'filename': name,
+                    'url': rel_path,
+                    'path': path,
+                    'name': name,
+                    'mtime': mtime,
+                })
+    return files
+
+
+def compare_entries(x, y):
+    result = cmp(-x['mtime'], -y['mtime'])
+    if result == 0:
+        return -cmp(x['filename'], y['filename'])
+    return result
+
+
+def write_file(url, data):
+    path = os.path.join(DESTINATION, url)
+    dirs = os.path.dirname(path)
+    if not os.path.isdir(dirs):
+        os.makedirs(dirs)
+    file = open(path, "w")
+    file.write(data.encode('UTF-8'))
+    file.close()
+    print '=>', path
+
+
+def is_html(name):
+    if name.endswith('.html') or name.endswith('.htm'):
+        return True
+
+
+@step
+def render_htmls(f, e):
+    for file in f:
+        if is_html(file['name']):
+            print '#', file['url']
+            rendered = renderTemplate(file['content'], file)
+            write_file(file['url'], rendered)
+
+
+def main():
+    print "Chiseling..."
+    ensure_destination()
+    print "\tReading files...",
+    files = sorted(get_tree(SOURCE), cmp=compare_entries)
+    print "Done."
+    print "\tRunning steps..."
+    for step in _STEPS:
+        print "\t\t",
+        step(files, jinja2_env)
+    print "\tDone."
+    print "Done."
+
+
+if __name__ == "__main__":
+    sys.exit(main())
